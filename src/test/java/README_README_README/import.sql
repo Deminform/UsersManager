b7ab58fb263c11216f645d8429c261d091d22c13
Demchenko Sergey <deminform@gmail.com>
2016-05-03 01:15:09 +0300

    ПОСЛЕДНИЙ ШРИХ ))

---
+++ src/test/java/README_README_README/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO test (name, age, isAdmin)
+VALUES ("Vovan", 30, 0), ("Koban", 25, 0), ("Lelik", 19, 1), ("Afanasiy", 30, 1), ("Alex", 20, 0), ("Mavrody", 26, 1),
+  ("Karabas", 20, 0), ("Tolik", 21, 1), ("Balda", 31, 1), ("Roja", 22, 0), ("Siroja", 23, 1);
